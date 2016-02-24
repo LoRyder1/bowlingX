4188c9be66835a62203b04e28cd6d9a26ce05297
Lovinder Pnag <lspnag@gmail.com>
2016-02-24 05:08:20 -0500

    rspec test for one strike and bonuses

--- spec/bowling_spec.rb
+++ spec/bowling_spec.rb
@@ -10,6 +10,10 @@ describe 'Bowling Game' do
     subject.roll 5
   end
 
+  def score_strike
+    subject.roll 10
+  end
+
   describe '#roll' do
 
     it 'is a method defined in Bowling Game' do
@@ -21,20 +25,31 @@ describe 'Bowling Game' do
     end
   end
 
-  it 'a gutter game results in zero' do
-    roll_many 20,0
-    expect(subject.score).to eq 0
-  end
+  describe 'complete games' do
 
-  it 'a game of all ones' do
-    roll_many 20,1
-    expect(subject.score).to eq 20
-  end
+    it 'a gutter game results in zero' do
+      roll_many 20,0
+      expect(subject.score).to eq 0
+    end
 
-  it 'a game with one spare' do
-    score_spare
-    subject.roll 4
-    roll_many 17,0
-    expect(subject.score).to eq 18
+    it 'a game of all ones' do
+      roll_many 20,1
+      expect(subject.score).to eq 20
+    end
+
+    it 'a game with one spare' do
+      score_spare
+      subject.roll 4
+      roll_many 17,0
+      expect(subject.score).to eq 18
+    end
+
+    it 'a game with one strike' do
+      score_strike
+      subject.roll 4
+      subject.roll 4
+      roll_many 17,0
+      expect(subject.score).to eq 26
+    end
   end
  end
